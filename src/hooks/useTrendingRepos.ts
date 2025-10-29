85b238c8ab093bdb2194697775fed9f5c6d7d6a0
Jeremy McCulley <jeremy.mcculley001@gmail.com>
2025-10-29 09:53:35 -0400

    Creating my query hook

---
+++ src/hooks/useTrendingRepos.ts
@@ -0,0 +1,16 @@
+import {useQuery} from '@tanstack/react-query';
+import type {RepoData, ChartData} from '../types';
+import {fetchTrendingReposApi} from '../api/github';
+// Data processing logic
+export const processRepoDataForChart = (repos: RepoData[]): ChartData[] =>{
+   const languageMap = repos.reduce((acc, repo) => {
+    if(repo.language){
+        acc[repo.language] = (acc[repo.language] || 0) + 1;
+    }
+    return acc;
+   }, {} as Record<string, number>);
+   return Object.entries(languageMap)
+   .map(([name, count]) => ({name, count}))
+   .sort((a, b) => b.count - a.count)
+   .slice(0, 10); 
+};
