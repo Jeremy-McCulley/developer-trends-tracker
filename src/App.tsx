700bcbbef1578400dd331c3d5452149e03cc3667
jeremymcculley <50459396+jeremymcculley@users.noreply.github.com>
2025-10-29 15:54:44 -0400

    more less added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import './App.less';
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <header>
+        <div className='logoContainer'>
+          <img src='./public/black-orange-white-logo-transparent.png' alt='Jeremy McCulley Freelance Logo'/>
+        </div>
+      </header>
+      <main>
+        <Dashboard />
+      </main>
     </div>
   );
 }
